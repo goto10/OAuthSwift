07d4aa3a0a8d58a0bef62073b747ee00848c33a9
Mike Anelli <myke_a@mac.com>
2014-08-06 15:43:00 -0700

    Fixed nil checks for Beta5 release.

--- OAuthSwift/extension/NSURL+OAuthSwift.swift
+++ OAuthSwift/extension/NSURL+OAuthSwift.swift
@@ -22,7 +22,7 @@ extension NSURL {
             absoluteURLString = (absoluteURLString as NSString).substringToIndex(absoluteURLString.utf16Count - 1)
         }
 
-        let URLString = absoluteURLString + (absoluteURLString.rangeOfString("?") ? "&" : "?") + queryString
+        let URLString = absoluteURLString + (absoluteURLString.rangeOfString("?") != nil ? "&" : "?") + queryString
 
         return NSURL(string: URLString)
     }
